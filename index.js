2e18f354b70b09b2d87b92f7c9d7fe12a2c4297f
Po-Cheng Yeh <blp100x@hotmail.com>
2023-10-02 23:41:36 +0800

    feat: display country list by default

--- index.js
+++ index.js
@@ -22,8 +22,107 @@ function hideloader() {
   document.getElementById("loading").style.display = "none";
 }
 // Function to define innerHTML for HTML table
-const showData = (data) => {
-  console.log(data);
+const showData = (countries) => {
+  const countryList = document.getElementById("countryList");
+
+  countries.map((country) => {
+    const { flags, name, population, region, capital } = country;
+    // country wrapper
+    const div = document.createElement("div");
+    div.classList.add(
+      "h-[336px]",
+      "w-full",
+      "overflow-hidden",
+      "rounded",
+      "bg-white",
+    );
+
+    // flag
+    const flagDiv = document.createElement("div");
+    flagDiv.classList.add(
+      "h-2/5",
+      "w-full",
+      "items-center",
+      "bg-contain",
+      "bg-center",
+      "bg-no-repeat",
+      "p-2",
+      "hover:bg-contain",
+      "md:bg-cover",
+    );
+    flagDiv.style.backgroundImage = `url(${flags.svg})`;
+
+    div.appendChild(flagDiv);
+    countryList.appendChild(div);
+
+    // description wrapper
+    const descriptionDiv = document.createElement("div");
+    descriptionDiv.classList.add("p-6");
+
+    // add country name tag
+    const countryName = document.createElement("h2");
+    countryName.classList.add("text-lg", "font-semibold");
+    countryName.textContent = name.official;
+
+    // add country deatil wrapper
+    const countryDetailDiv = document.createElement("div");
+    countryDetailDiv.classList.add(
+      "mt-4",
+      "flex",
+      "flex-col",
+      "gap-2",
+      "text-sm",
+      "font-light",
+    );
+
+    const deatilArr = ["Population", "Region", "Capital"];
+
+    deatilArr.map((deatilTitle) => {
+      const paragraph = document.createElement("div");
+      const span = document.createElement("span");
+      span.classList.add("mr-1", "font-medium");
+      span.textContent = deatilTitle + ":";
+      paragraph.appendChild(span);
+
+      if (deatilTitle === "Population") {
+        paragraph.innerHTML += population;
+      } else if (deatilTitle === "Region") {
+        paragraph.innerHTML += region;
+      } else if (deatilTitle === "Capital") {
+        paragraph.innerHTML += capital[0];
+      }
+
+      countryDetailDiv.appendChild(paragraph);
+    });
+
+    descriptionDiv.appendChild(countryName);
+    descriptionDiv.appendChild(countryDetailDiv);
+    div.append(descriptionDiv);
+  });
+
+
+  /* the country list boilplate */
+  
+  // <div class="h-[336px] w-full overflow-hidden rounded bg-white">
+  //   <div
+  //     class="h-2/5 w-full items-center bg-contain bg-center bg-no-repeat p-2 hover:bg-contain md:bg-cover"
+  //     style="background-image: url(https://flagcdn.com/as.svg)"
+  //   ></div>
+  //   <div class="p-6">
+  //     <h2 class="text-lg font-semibold">American Samoa</h2>
+  //     <div class="mt-4 flex flex-col gap-2 text-sm">
+  //       <div class="font-light">
+  //         <span class="mr-1 font-medium">Population:</span>323,947,000
+  //       </div>
+  //       <div class="font-light">
+  //         <span class="mr-1 font-medium">Region:</span>Americas
+  //       </div>
+  //       <div class="font-light">
+  //         <span class="mr-1 font-medium">Capital:</span>Washington, D.C.
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
 };
 getapi(api_url + apiFetchList.searchByName + "amer");
 
